43f8dea591f51d268ed6c56dd2c8d481118569d5
Cameron Higby-Naquin <cameronh@aweber.com>
2012-07-31 10:13:54 -0400

    Fix keypress javascript.

--- cmd.py
+++ cmd.py
@@ -1,9 +1,21 @@
+import os
 import json
 from flask import Flask
 app = Flask(__name__)
 
 # TODO: 'random', 'menu' commands.  possibly making sure startup
 # doesn't do bad things if it's already running.
+
+def _open_cmds():
+    paths = ['/home/chigby/webapps/cmd/', './']
+    for path in paths:
+        cmd_path = path + 'cmd.json'
+        if os.path.exists(path):
+            try:
+                f = open(cmd_path)
+            except IOError:
+                f = None
+    return f
 
 @app.route("/")
 def nothing():
@@ -12,10 +24,10 @@ def nothing():
 @app.route("/<cmd>")
 def cmd(cmd):
     try:
-        cmds = json.load(open('/home/chigby/webapps/cmd/cmd.json'))
+        cmds = json.load(_open_cmds())
     except ValueError:
         return 'Bad command or file name, but there might be a problem.'
     return cmds.get(cmd, 'Bad command or file name.')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=48600)
+    app.run(host='0.0.0.0', port=48600, debug=True)
--- prompt.js
+++ prompt.js
@@ -27,7 +27,29 @@ function makeResponse(cmd) {
     return http.responseText;
 }
 
+window.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    if (event.keyCode == 8) {
+        event.preventDefault();
+        var t = document.getElementById('text');
+        var len = t.innerHTML.length;
+        if (t.innerHTML.substring(len - 7, len - 2) !== '<br>$') {
+            t.innerHTML = t.innerHTML.substring(0, len - 2);
+            t.innerHTML += cursor;
+        }
+    }
+    return false;
+});
+
+window.addEventListener("keydown", function(e) {
+    console.log(e.keyCode);
+    if (e.keyCode === 8) {
+        console.log("here we go");
+        e.preventDefault();
+    }});
+
 window.addEventListener('keypress', function(event) {
+    event.preventDefault();
     var charCode = (typeof event.which == "number") ? event.which : event.keyCode;
     var c = String.fromCharCode(charCode);
     var t = document.getElementById('text');
@@ -38,6 +60,7 @@ window.addEventListener('keypress', function(event) {
             t.innerHTML = t.innerHTML.substring(0, len - 2);
             t.innerHTML += cursor;
         }
+        event.preventDefault();
         break;
     case 13: // Return
         var cmd = t.innerHTML.substring(t.innerHTML.lastIndexOf('$') + 2);
